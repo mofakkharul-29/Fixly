0628a43f3c56b0566bdf8dfe84940e564a3737c3
mofakkharul-29 <mofakkharulislam29.info@gmail.com>
2026-04-20 15:50:37 +0600

    user model completed|

---
+++ lib/features/auth/model/app_user.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fixly/features/auth/widget/user_role.dart';
+
+class AppUser {
+  final String uid;
+  final String email;
+  final String name;
+  final UserRole role;
+  final String? photoUrl;
+  final DateTime createdAt;
+
+  const AppUser({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.role,
+    this.photoUrl,
+    required this.createdAt,
+  });
+
+  factory AppUser.fromMap(Map<String, dynamic> json) {
+    return AppUser(
+      uid: json['uid'] as String,
+      email: json['email'] as String,
+      name: json['name'] as String,
+      role: json['role'] as UserRole,
+      photoUrl: json['photoUrl'],
+      createdAt: json['createdAt'] is Timestamp
+          ? (json['createdAt'] as Timestamp).toDate()
+          : DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  AppUser copyWith({
+    String? uid,
+    String? email,
+    String? name,
+    UserRole? role,
+    DateTime? createdAt,
+    String? photoUrl,
+  }) {
+    return AppUser(
+      uid: uid ?? this.uid,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      role: role ?? this.role,
+      createdAt: createdAt ?? this.createdAt,
+      photoUrl: photoUrl ?? this.photoUrl,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'photoUrl': photoUrl,
+      'role': role,
+      'createdAt': createdAt,
+    };
+  }
+}
